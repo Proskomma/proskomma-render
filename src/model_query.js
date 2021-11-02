c48e3cfd4e12e8a5531e9328477af52af54990d7
Mark Howe <mvahowe@gmail.com>
2021-11-02 12:00:38 +0100

    Optionally filter model query by docSet and doc

--- src/model_query.js
+++ src/model_query.js
@@ -1,11 +1,14 @@
-const doModelQuery = async pk => {
+const doModelQuery = async (pk, docSetIds, documentIds) => {
+    // Empty arrays mean all docSets/documents
+    docSetIds = docSetIds || [];
+    documentIds = documentIds || [];
     const result = await pk.gqlQuery(
         '{' +
-        '  docSets {' +
+        `  docSets${docSetIds.length === 0 ? '' : `(ids:[${docSetIds.map(ds => `"${ds}"`).join(', ')}])`} {` +
         '    id' +
         '    selectors { key value }' +
         '    tags' +
-        '    documents {' +
+        `    documents${documentIds.length === 0 ? '' : `(ids:[${documentIds.map(d => `"${d}"`).join(', ')}])`} {` +
         '      id' +
         '      headers { key value }' +
         '      idParts { type parts }' +
--- src/test/code/hello.js
+++ src/test/code/hello.js
@@ -7,6 +7,17 @@ const {
     ScriptureParaModelQuery
 } = require('../../../');
 
+const { pkWithDocs } = require('../lib/load');
+const pk = pkWithDocs(
+    [
+        ['../test_data/rut.usfm', {lang: "eng", abbr: "webbe"}],
+        ['../test_data/est.usfm', {lang: "eng", abbr: "webbe"}],
+        ['../test_data/rut.usfm', {lang: "eng", abbr: "webbe2"}],
+    ],
+    {}
+);
+let globalResult;
+
 const testGroup = "Basics";
 
 test(
@@ -17,6 +28,51 @@ test(
             t.doesNotThrow(() => new ScriptureParaModel);
             t.doesNotThrow(() => new ScriptureDocSet);
             t.doesNotThrow(() => new ScriptureParaDocument);
+        } catch (err) {
+            console.log(err);
+        }
+    },
+);
+
+test(
+    `ScriptureParaModelQuery with one arg (${testGroup})`,
+    async function (t) {
+        try {
+            t.plan(3);
+            globalResult = await ScriptureParaModelQuery(pk);
+            t.equal(globalResult.docSets.length, 2);
+            t.equal(globalResult.docSets[0].documents.length, 2);
+            t.equal(globalResult.docSets[1].documents.length, 1);
+        } catch (err) {
+            console.log(err);
+        }
+    },
+);
+
+test(
+    `ScriptureParaModelQuery with two args (${testGroup})`,
+    async function (t) {
+        try {
+            t.plan(2);
+            const resultData = await ScriptureParaModelQuery(pk, ['eng_webbe']);
+            t.equal(resultData.docSets.length, 1);
+            t.equal(resultData.docSets[0].documents.length, 2);
+        } catch (err) {
+            console.log(err);
+        }
+    },
+);
+
+test(
+    `ScriptureParaModelQuery with three args (${testGroup})`,
+    async function (t) {
+        try {
+            t.plan(2);
+            const docId = globalResult.docSets[0].documents[0].id;
+            const resultData = await ScriptureParaModelQuery(pk, ['eng_webbe'], [docId]);
+            t.equal(resultData.docSets.length, 1);
+            t.equal(resultData.docSets[0].documents.length, 1);
+            // console.log(JSON.stringify(resultData, null, 2));
         } catch (err) {
             console.log(err);
         }
